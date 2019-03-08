1a983eddb7391d7b135ddab3c2da42d2377bf259
jeanfern5 <jeanfern555@gmail.com>
2019-03-08 11:15:31 -0600

    Implemented parsing the input URL, completed code in parse_url() from client.c

--- src/client.c
+++ src/client.c
@@ -31,24 +31,26 @@ urlinfo_t *parse_url(char *url)
   char *hostname = strdup(url);
   char *port;
   char *path;
+  char *backlash, *colon;
 
   urlinfo_t *urlinfo = malloc(sizeof(urlinfo_t));
+  
+  // We can parse the input URL by doing the following:
+  
+  // 1. Use strchr to find the first backslash in the URL (this is assuming there is no http:// or https:// in the URL).
+  backlash = strchr(hostname, '/');
+  // 2. Set the path pointer to 1 character after the spot returned by strchr.
+  path = backlash + 1;
+  // 3. Overwrite the backslash with a '\0' so that we are no longer considering anything after the backslash.
+  *backlash = '\0';
 
-  /*
-    We can parse the input URL by doing the following:
-
-    1. Use strchr to find the first backslash in the URL (this is assuming there is no http:// or https:// in the URL).
-    2. Set the path pointer to 1 character after the spot returned by strchr.
-    3. Overwrite the backslash with a '\0' so that we are no longer considering anything after the backslash.
-    4. Use strchr to find the first colon in the URL.
-    5. Set the port pointer to 1 character after the spot returned by strchr.
-    6. Overwrite the colon with a '\0' so that we are just left with the hostname.
-  */
-
-  ///////////////////
-  // IMPLEMENT ME! //
-  ///////////////////
-
+  // 4. Use strchr to find the first colon in the URL.
+  colon = strchr(hostname, ':');
+  // 5. Set the port pointer to 1 character after the spot returned by strchr.
+  port = colon + 1;
+  // 6. Overwrite the colon with a '\0' so that we are just left with the hostname.
+  *port = '\0';
+ 
   return urlinfo;
 }
 
@@ -85,17 +87,15 @@ int main(int argc, char *argv[])
     exit(1);
   }
 
-  /*
-    1. Parse the input URL
-    2. Initialize a socket by calling the `get_socket` function from lib.c
-    3. Call `send_request` to construct the request and send it
-    4. Call `recv` in a loop until there is no more data to receive from the server. Print the received response to stdout.
-    5. Clean up any allocated memory and open file descriptors.
-  */
+  
+  // 1. Parse the input URL
+  urlinfo_t *ulrinfo_t = parse_url(argv[1]);
+  // 2. Initialize a socket by calling the `get_socket` function from lib.c
+  // 3. Call `send_request` to construct the request and send it
+  // 4. Call `recv` in a loop until there is no more data to receive from the server. Print the received response to stdout.
+  // 5. Clean up any allocated memory and open file descriptors.
 
-  ///////////////////
-  // IMPLEMENT ME! //
-  ///////////////////
+
 
   return 0;
 }
